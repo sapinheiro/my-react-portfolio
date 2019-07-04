b741e7afc1e817475fc77be8b1b833316b0a596a
sapinheiro <sampinheiro9@gmail.com>
2019-07-04 15:29:49 -0400

    Updates

---
+++ precache-manifest.549cbe246e82a0cf89a07c1df9846907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ff7d4b2c4de6520395b80c939d176d",
+    "url": "sampinheiro.com/index.html"
+  },
+  {
+    "revision": "dec7dd4fc055361d4476",
+    "url": "sampinheiro.com/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "sampinheiro.com/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dec7dd4fc055361d4476",
+    "url": "sampinheiro.com/static/js/main.06ec5d48.chunk.js"
+  },
+  {
+    "revision": "dcbe6d4a394b12290939",
+    "url": "sampinheiro.com/static/js/runtime~main.2233e90f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "sampinheiro.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
